2ba67a56652a80d2bc2ee222b880aba287b362f3
mehdi <mehdijafarzadehh4@gmail.com>
2023-07-04 14:53:01 +0330

    nav set 4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Navbar.css"
-
 const Navbar = () => {
     return (
         <div className="nav-container">
             <ul className="nav-ul">
-                <li className="nav-li">item</li>
-                <li className="nav-li">item</li>
-                <li className="nav-li">item</li>
-                <FontAwesomeIcon icon={["fal", "coffee"]} />
+                <li className="nav-li"></li>
+                <li className="nav-li">Home</li>
+                <li className="nav-li">shop</li>
+                <li className="nav-li">About us</li>
+                <li className="nav-li">shoping card</li>
+                
             </ul>
 
         </div>
