0bbacc75ce1316f4535fe87e583e2ada42191240
mehdi <mehdijafarzadehh4@gmail.com>
2023-07-05 13:40:49 +0330

    footer v.1 complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,19 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="Footer">
-            <div className="footer-up"></div>
-            <div className="footer-down"></div>
+            <div className="footer-up">
+                <p>Webco programming group </p>
+            </div>
+            <div className="footer-down">
+                <p>@copyright webco</p>
+                <div className="footer-down-mid">
+                    <p><img className="footer-icons" src="./assets/footer/youtube.png"  alt="youtube icon" /></p>
+                    <p><img className="footer-icons" src="./assets/footer/instagram.png"  alt="instagram icon" /></p>
+                    <p><img className="footer-icons" src="./assets/footer/telegram.png"  alt="telegram icon" /></p>
+                    <p><img className="footer-icons" src="./assets/footer/github.png"  alt="github icon" /></p>
+                </div>
+                <p>world of programming</p>
+            </div>
         </div>
     )
 }
